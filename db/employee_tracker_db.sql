b285f799bef72fb1ca099639bd58d3b94d95d510
Yousef Shahin <shahinyousef03@gmail.com>
2021-03-28 14:59:03 -0600

    schema created

---
+++ db/employee_tracker_db.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS employeetracker_db;
+CREATE database employeetracker_db;
+USE employeetracker_db;
+
+CREATE TABLE employee (
+	id INT NOT NULL AUTO_INCREMENT,
+	first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE department (
+	id INT NOT NULL AUTO_INCREMENT,
+    department_name VARCHAR (30),
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE role (
+	id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30),
+	salary INT,
+    department_id INT,
+    PRIMARY KEY(id)
+);
